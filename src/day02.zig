64b68d0effc8b52c424ab0fdc2590024751cc524
Elias Erke <elias.erke@proton.me>
2024-12-03 09:29:31 +0200

    day 2

--- src/day02.zig
+++ src/day02.zig
@@ -10,33 +10,92 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day02.txt");
 
+fn isSafe(line: []const i32) bool {
+    // 
+    var isAsc = true;
+
+    for (0..line.len-1, 1..line.len) |i, j|{
+        if (j == line.len) {
+            return true;
+        }
+        if (line[i] == line[j]) {
+            return false;
+        }
+        if (i == 0)  {
+            if (line[i] > line[j]) {
+                isAsc = false;
+            }
+        }
+        if (@abs(line[i] - line[j]) > 3) {
+            return false;
+        }
+        if (isAsc and line[i] > line[j]) {
+            return false;
+        } else if (!isAsc and line[i] < line[j]) {
+            return false;
+        }
+
+
+    }
+    return true;
+}
+
 pub fn main() !void {
     var arena_state = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena_state.deinit();
     const arena = arena_state.allocator();
 
-    print("Part 1: {d}", part1(arena, data));
-    print("Part 2: {d}", part2(arena, data));
+    print("Part 1: {d}\n", .{part1(arena, data) catch {return;}});
+    print("Part 2: {d}\n", .{part2(arena, data) catch {return;}});
 }
 
 fn part1(allocator: std.mem.Allocator, input: []const u8) !i32 {
     // Start a loop through the lines of the input
     var inputLines = std.mem.tokenizeScalar(u8, input, '\n');
+    var safe: i32 = 0;
     while (inputLines.next()) |line| {
-        _ = line;
-    }
+        var nums = std.mem.tokenizeScalar(u8,  line, ' ');
+        var row = std.ArrayList(i32).init(allocator);
 
-    _ = allocator;
+        while (nums.next()) |num| {
+            try row.append(try parseInt(i32, num, 10));
+        }
+        if (isSafe(row.items)) {
+            safe += 1;
+        }
+
+    }
+    return safe;
 }
 
 fn part2(allocator: std.mem.Allocator, input: []const u8) !i32 {
     // Start a loop through the lines of the input
     var inputLines = std.mem.tokenizeScalar(u8, input, '\n');
+    var safe: i32 = 0;
     while (inputLines.next()) |line| {
-        _ = line;
-    }
+        var nums = std.mem.tokenizeScalar(u8,  line, ' ');
+        var row = std.ArrayList(i32).init(allocator);
 
-    _ = allocator;
+        while (nums.next()) |num| {
+            try row.append(try parseInt(i32, num, 10));
+        }
+        if (isSafe(row.items)) {
+            safe += 1;
+        } else {
+            var i: usize = 0;
+            while(i < row.items.len) {
+                const oldVal = row.orderedRemove(i);
+                if (isSafe(row.items)) {
+                    safe += 1;
+                    break;
+                }
+                try row.insert(i, oldVal);
+                i+=1;
+            }
+        }
+
+    }
+    return safe;
 }
 
 // Useful stdlib functions
@@ -76,10 +135,19 @@ test "part 1 example" {
     const arena = arena_state.allocator();
 
     const example_input =
-        \\
+        \\7 6 4 2 1 2
+        \\1 2 4 6 6
+        \\9 7 6 2 1
+        \\1 13 14 17 19
+        \\18 6 4 2 1
+        \\1 3 6 7 9
+        \\10 12 13 16 17
+        \\14 12 10 7 5 3
+        \\47 49 50 52 53 54 57 59
+        \\28 29 32 33 34
     ;
 
-    try std.testing.expectEqual(0, try part1(arena, example_input));
+    try std.testing.expectEqual(5, try part1(arena, example_input));
 }
 
 test "part 2 example" {
@@ -88,8 +156,14 @@ test "part 2 example" {
     const arena = arena_state.allocator();
 
     const example_input =
-        \\
+        \\7 6 4 2 1
+        \\1 2 7 8 9
+        \\9 7 6 2 1
+        \\1 3 2 4 5
+        \\8 6 4 4 1
+        \\1 3 6 7 9
+
     ;
 
-    try std.testing.expectEqual(0, try part2(arena, example_input));
+    try std.testing.expectEqual(4, try part2(arena, example_input));
 }
